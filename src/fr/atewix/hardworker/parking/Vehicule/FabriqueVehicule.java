0a5969fd7269261868be5e0c7f72fb485f6fc724
Jafar13 <kevin.duglue@gmail.com>
2015-01-18 23:21:48 +0100

    JavaDoc FabriqueVehicule

--- src/fr/atewix/hardworker/parking/Vehicule/FabriqueVehicule.java
+++ src/fr/atewix/hardworker/parking/Vehicule/FabriqueVehicule.java
@@ -2,7 +2,22 @@ package fr.atewix.hardworker.parking.Vehicule;
 
 import fr.atewix.hardworker.parking.business.Client;
 
+/**
+ * Class FabriqueVehicule, qui implement l'interfacte IFabriqueVehicule
+ * @see fr.atewix.hardworker.parking.Vehicule.FabriqueVehicule
+ * @author Lucas Debiasi, Micheal Gileta, Sylvain De Barros, Kevin Duglue
+ */
 public class FabriqueVehicule implements IFabriqueVehicule {
+
+    /**
+     * Methode qui permet de créer un vehicule, en fonction de son type
+     * @param typeVehicule
+     * @param immatriculation
+     * @param proprietaire
+     * @param marque
+     * @param modele
+     * @return Vehicule crée
+     */
     @Override
     public Vehicule Creer(String typeVehicule, String immatriculation, Client proprietaire, String marque, String modele) {
         if(typeVehicule == "Voiture")
@@ -11,7 +26,6 @@ public class FabriqueVehicule implements IFabriqueVehicule {
             return new Moto(immatriculation, proprietaire, marque, modele);
         else if (typeVehicule == "Camion")
             return new Camion(immatriculation, proprietaire, marque, modele);
-        // message d'erreur a mettre
         System.exit(0);
         return null;
     }
