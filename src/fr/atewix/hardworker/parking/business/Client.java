f235fb9d7cd78ac24b0ee1c301281bb49d0fd3d7
Jafar13 <kevin.duglue@gmail.com>
2015-01-18 22:36:17 +0100

    Debut Javadoc

--- src/fr/atewix/hardworker/parking/business/Client.java
+++ src/fr/atewix/hardworker/parking/business/Client.java
@@ -3,13 +3,43 @@ package fr.atewix.hardworker.parking.business;
 import fr.atewix.hardworker.parking.Vehicule.Vehicule;
 import java.util.ArrayList;
 
+/**
+ * Class Client : permet de créer un objet client contenant toutes les informations propres à un client.
+ * @author Lucas Debiasi, Micheal Gileta, Sylvain De Barros, Kevin Duglue
+ */
 public class Client {
+
+	/**
+	 * Prenom du client
+	 */
 	private String prenom;
+
+	/**
+	 * Nom du client
+	 */
 	private String nom;
+
+	/**
+	 * Adresse du client
+	 */
 	private String adresse;
+
+	/**
+	 * Liste des vehicules appartenant à ce client
+	 */
 	private ArrayList<Vehicule> listeVehiculeClient;
+
+	/**
+	 * Liste des reservations du client
+	 */
 	private ArrayList<Reservation> listeReservation;
 
+	/**
+	 * Constructeur de la classe Client, qui crée un client à partir d'un prenom, d'un nom et d'une adresse
+	 * @param prenom
+	 * @param nom
+	 * @param adresse
+	 */
 	public Client(String prenom, String nom, String adresse) {
 		this.prenom = prenom;
 		this.nom = nom;
@@ -17,33 +47,64 @@ public class Client {
 		listeVehiculeClient = new ArrayList<Vehicule>();
 	}
 
+	/**
+	 * Methode permettant de mettre dans une String toutes les informations utiles à un client
+	 * @return
+	 */
 	public String toString() {
 		return  nom + " " + prenom;
 	}
 
+	/**
+	 * Methode permettant d'ajouter un vehicule à la liste des véhicules du client
+	 * @param vehicule
+	 */
 	public void addVehicule(Vehicule vehicule){
 		listeVehiculeClient.add(vehicule);
 	}
 
-
+	/**
+	 * Methode permettant d'obtenir la liste des véhicules du client
+	 * @return Liste des véhicules du client
+	 */
 	public ArrayList<Vehicule> getListeVehiculeClient() {
 		return this.listeVehiculeClient;
 	}
 
+	/**
+	 * Methode permettant d'obtenir le prenom du client
+	 * @return Prenom du client
+	 */
 	public String getPrenom() {
 		return prenom;
 	}
 
+	/**
+	 * Methode permettant d'obtenir le nom du client
+	 * @return Nom du client
+	 */
 	public String getNom() {
 		return nom;
 	}
 
+	/**
+	 * Methode permettant d'obtenir l'adresse du client
+	 * @return Adresse du client
+	 */
 	public String getAdresse() { return adresse;}
 
+	/**
+	 * Methode permettant d'obtenir la liste des reservations du client
+	 * @return Liste des reservations du client
+	 */
 	public ArrayList<Reservation> getListeReservation(){
 		return this.listeReservation;
 	}
 
+	/**
+	 * Methode permettant d'ajouter une reservation à la liste des reservations du client
+	 * @param reservation
+	 */
 	public void addReservation(Reservation reservation){
 		listeReservation.add(reservation);
 	}
--- src/fr/atewix/hardworker/parking/facture/Facture.java
+++ src/fr/atewix/hardworker/parking/facture/Facture.java
@@ -9,37 +9,94 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.GregorianCalendar;
 
-
+/**
+ * Class Facture : permet de fabriquer un objet facture à chaque sortie de vehicule
+ * @author Lucas Debiasi, Micheal Gileta, Sylvain De Barros, Kevin Duglue
+ */
 public class Facture {
+
+	/**
+	* Variable statique permettant de donner un numéro unique à chaques factures crées
+	*/
 	private static int numero = 0;
+
+	/**
+	 * Le numéro de la facture
+	 */
 	private int numeroFacture;
+
+	/**
+	 * Vehicule concerné pour la facture
+	 */
     private Vehicule vehiculeconcerne;
+
+	/**
+	 * Montant total de la facture
+	 */
     private double montantfacture;
+
+	/**
+	 * Date d'arrivée du vehicule au parking
+	 */
     private Calendar datedebut;
+
+	/**
+	 * Date de sortie du vehicule du parking
+	 */
     private Calendar datefin;
 
-	private double TVA = 0.196;
+	/**
+	 * Valeur de la TVA constante
+	 */
+	private final double TVA = 0.196;
 
-    public Facture(Vehicule voiture, Calendar datedebut, int tarifhoraire){
+	/**
+	 * Constructeur de la classe facture, qui permet de créer une facture à partir d'un vehicule, d'une date de debut et d'un tarif
+	 * horaire.
+	 * @param vehicule
+	 * @param datedebut
+	 * @param tarifhoraire
+	 */
+    public Facture(Vehicule vehicule, Calendar datedebut, int tarifhoraire){
         ++this.numero;
         this.numeroFacture = this.numero;
-        this.vehiculeconcerne = voiture;
+        this.vehiculeconcerne = vehicule;
         this.datedebut = datedebut;
         this.datefin = new GregorianCalendar();
         this.montantfacture = calculMontantTTC(datedebut, datefin, tarifhoraire, TVA);
     }
 
+	/**
+	 * Methode permettant de calculer le montant de la facture HT, à partir de la date de debut, date de fin
+	 * et du tarif horaire.
+	 * @param datedebut
+	 * @param datefin
+	 * @param tarifhoraire
+	 * @return Montant de la facture Hors Taxes
+	 */
     private double calculMontantHT(Calendar datedebut, Calendar datefin, int tarifhoraire){
         return tarifhoraire + (datefin.get(Calendar.DAY_OF_MONTH) - datedebut.get(Calendar.DAY_OF_MONTH))*24*tarifhoraire
                 + (datefin.get(Calendar.HOUR_OF_DAY)-datedebut.get(Calendar.HOUR_OF_DAY))*tarifhoraire
                 + ((datefin.get(Calendar.MINUTE) + datedebut.get(Calendar.MINUTE))/60)*tarifhoraire;
     }
-    
+
+	/**
+	 * Methode permettant de calculer le montant TTC de la facture
+	 * @param datedebut
+	 * @param datefin
+	 * @param tarifhoraire
+	 * @param TVA
+	 * @return Montant de la facture Toutes taxes comprises
+	 */
     private double calculMontantTTC(Calendar datedebut, Calendar datefin, int tarifhoraire, double TVA){
     	double resultatHT = calculMontantHT(datedebut, datefin, tarifhoraire);
     	return (resultatHT*TVA)+resultatHT;
     }
-    
+
+	/**
+	 * Methode qui permet de placer dans une String toutes les informations utiles à l'objet
+	 * @return Informations souhaitées de l'objet
+	 */
     public String toString() {
 		return "Facture numero = " + numeroFacture + "\r\n"
 			    + vehiculeconcerne.getProprietaire().getNom()+ " " + vehiculeconcerne.getProprietaire().getPrenom()+ "\r\n"  
@@ -56,7 +113,10 @@ public class Facture {
 				+ "TVA=" + TVA*100 + "%" + "\r\n"
 				+ "Total :" + arrondirMontant(montantfacture) + " euros";
 	}
-    
+
+	/**
+	 * Methode permettant d'enregistrer une facture dans un fichier externe à l'application
+	 */
     public void Enregistrer(){
     	String nomFacture = "Facture_" + this.numeroFacture+".txt";
     	try {
@@ -69,6 +129,11 @@ public class Facture {
 		}
     }
 
+	/**
+	 * Methode permettant d'arrondir au dixième près une valeure.
+	 * @param val
+	 * @return Valeur arrondie
+	 */
 	public double arrondirMontant(double val) {
 		return (Math.floor(val*100.0))/100;
 	}
